806faeaf99e04e97feada95e521b11efbac21a08
mrwafflessss <isagri04@kingshigh.school.nz>
2024-08-29 11:01:41 +1200

    1.3
    stable with bundrys to sop player from moving outside the map

--- Intro.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-func _ready():
-	
-	$AnimationPlayer.play("Fade in")
-	Wait(get_tree().create_timer(6), "timeout")
-	$AnimationPlayer.play("Fade out")
-	Wait(get_tree().create_timer(3), "timeout")
-	get_tree().change_scene("res//MainMenu.tscn")
--- title_screen.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-
-func _on_start_button_pressed() -> void:
-	get_tree().scene("res://Scenes/MainScene.tscn")
-	pass # Replace with function body.
