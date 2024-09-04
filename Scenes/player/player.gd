f148904fa95e61def2a5ea3fa33b3cc5e3f7cfec
mrwafflessss <isagri04@kingshigh.school.nz>
2024-09-05 09:59:36 +1200

    1.4 stable
    new sound track

--- Scenes/player/player.gd
+++ Scenes/player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @onready var direction = Vector2.ZERO
-@export var SPEED = 180.0
+@export var SPEED = 190.0
 @export var ACCELERATION = 20.0
 @export var FRICTION = 10.0
 const BULLET_1 = preload("res://bullet_1.tscn")
@@ -10,21 +10,23 @@ const BULLET_1 = preload("res://bullet_1.tscn")
 
 
 func _physics_process(_delta):
+	look_at(get_global_mouse_position())
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_vector("left","right","up"," down").normalized()
+	direction = Input.get_vector("left","right","up","down").normalized()
 	if direction:
 		velocity = velocity.move_toward(direction * SPEED, ACCELERATION)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION)
 	
 
-	sprite.look_at(get_global_mouse_position())
+	#sprite.look_at(get_global_mouse_position())
 	if Input.is_action_just_released("shoot"):
 		var bullet = BULLET_1.instantiate()
 		bullet.global_position = global_position
 		bullet.rotate(direction.angle())
 		world.add_child(bullet)
+		#sprite.look_at(get_global_mouse_position())
 		bullet.look_at(get_global_mouse_position())
 		
 
