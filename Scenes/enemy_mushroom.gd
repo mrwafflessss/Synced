41935ab2d5aabe311027b62008b520ef10d820d3
mrwafflessss <isagri04@kingshigh.school.nz>
2024-09-17 12:25:19 +1200

    1.4 stable
    zombie noise effect

--- Scenes/enemy_mushroom.gd
+++ Scenes/enemy_mushroom.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 115.0
+const SPEED = 100.0
 const JUMP_VELOCITY = -200.0
 @onready var sprite = $sprite
 @onready var player = get_tree().get_first_node_in_group("Player")
